31fec2d7a2b021f1f6db46d56a955e6827a5b8cf
Lia Chin-Purcell <lia_chinpurcell@berkeley.edu>
2022-04-24 15:19:32 +0000

    adding utils

--- ligotools/tests/tests_utils.py
+++ ligotools/tests/tests_utils.py
@@ -1,10 +1,15 @@
 from utils import *
 
 def test_whiten():
+	strain_H1 = 
+	psd_H1 = 
+	dt = 
     strain_H1_whiten = whiten(strain_H1,psd_H1,dt)
 
 def test_write_wavfile():
     filename = "test"
+	fs = 1
+	data = None
     write_wavfile(filename,fs,data)
 
     
